dc50b8828faa90ce93db385c1493c371a4814132
Alexander Tagiltsev <alextagiltsev@yandex.ru>
2023-10-12 13:26:59 +0300

    Initial commit

---
+++ scripts/myAgeController.js
@@ -0,0 +1,6 @@
+﻿angular.module('myApp', []).
+    controller('myAgeController', function ($scope) {
+        $scope.getAge = function () {
+            return $scope.birthYear ? (new Date().getFullYear() - $scope.birthYear.getFullYear()) + "," + (new Date().getMonth() - $scope.birthYear.getMonth()) : "Укажите месяц и год";
+        }
+    });
